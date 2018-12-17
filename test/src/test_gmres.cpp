7a3b36ec96559e823e699facaf13b7c79eb7bc95
Davide Lasagna <lasagnadavide@gmail.com>
2018-12-17 21:06:57 +0000

    add tests for lower banded matrix

--- test/src/test_gmres.cpp
+++ test/src/test_gmres.cpp
@@ -57,14 +57,16 @@ TEST_CASE("gmres-dvec", "tests") {
 
     // define local and global matrix
     Mat A = {{1, 2, 3, 4}, 2};
-    DMatrix<Mat, Vec, 0> Ad(MPI_COMM_WORLD, A, DMatrixBandType::UPPER);
+    DMatrix<Mat, Vec, 0> Ad_UP(MPI_COMM_WORLD, A, DMatrixBandType::UPPER);
+    DMatrix<Mat, Vec, 0> Ad_LO(MPI_COMM_WORLD, A, DMatrixBandType::LOWER);
 
     // now define the exact solution, locally, and globally
     Vec x = {1, 2};
     DVector<Vec, 0> xd(MPI_COMM_WORLD, x);
 
     // obtain right hand side bd = Ad * xd
-    DVector<Vec, 0> bd = Ad * xd;
+    DVector<Vec, 0> bd_UP = Ad_UP * xd;
+    DVector<Vec, 0> bd_LO = Ad_LO * xd;
 
     // now this should be the matrix
     // 1   3  -1   0   0   0   0   0
@@ -76,26 +78,36 @@ TEST_CASE("gmres-dvec", "tests") {
    // -1   0   0   0   0   0   1   3
     // 0  -1   0   0   0   0   2   4
     //
+    // (or the lower band version)
     // times the vector
     // [1 2 1 2 1 2 1 2]
     // 
     // which is the vector
     // [6 8 6 8 6 8 6 8]
+    // 
+    // in both cases
+    //
     // the same everywhere
 
-    REQUIRE( bd(0, TAG_HEAD()) == 6 );
-    REQUIRE( bd(1, TAG_HEAD()) == 8 );
+    REQUIRE( bd_UP(0, TAG_HEAD()) == 6 );
+    REQUIRE( bd_LO(0, TAG_HEAD()) == 6 );
+    REQUIRE( bd_UP(1, TAG_HEAD()) == 8 );
+    REQUIRE( bd_LO(1, TAG_HEAD()) == 8 );
 
     // now make a copy of bd and try to get x by solving
     // Ad * x = b
-    DVector<Vec, 0> sol = bd;
+    DVector<Vec, 0> sol_UP = bd_UP;
+    DVector<Vec, 0> sol_LO = bd_LO;
 
     // solve in place. This should quickly converge to machine accuracy 
     auto opts = GMRESOptions(1e-8, 2, true); 
-    solve_gmres(Ad, sol, opts);
+    solve_gmres(Ad_UP, sol_UP, opts);
+    solve_gmres(Ad_LO, sol_LO, opts);
 
     // subtract exact solution, this should be small
-    DVector<Vec, 0> err = sol - xd;
+    DVector<Vec, 0> err_UP = sol_UP - xd;
+    DVector<Vec, 0> err_LO = sol_LO - xd;
 
-    REQUIRE(norm(err) / norm(bd) < 1e-8);
+    REQUIRE(norm(err_UP) / norm(bd_UP) < 1e-8);
+    REQUIRE(norm(err_LO) / norm(bd_LO) < 1e-8);
 }
